8eed705d34d1e840307635b9cda6b3c052463279
haydenykh <yonghayden68@gmail.com>
2024-11-08 13:30:34 +0800

    feat: first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+def Run():
+    # Welcome message
+    print("""
++ =============== +
++                 +
++  Calculator.py  +
++                 +
++ =============== +
+    """)
+
+    # Get input
+    n1 = input("Enter the first number: ")
+    n2 = input("Enter the second number: ")
+
+    # Pick options
+    title = "Please select your operator."
+    options = ("+", "-", "*", "/", "**", "//", "%")
+    operator = input(title + "\n" + str(options))
+
+    try:
+        n1 and n2
+    except NameError:
+        print("Input 1 and Input 2 has no inputs.")
+    else:
+        if operator == "+":
+            result = int(n1) + int(n2)
+        elif operator == "-":
+            result = int(n1) - int(n2)
+        elif operator == "*":
+            result = int(n1) * int(n2)
+        elif operator == "/":
+            result = int(n1) / int(n2)
+        elif operator == "**":
+            result = int(n1) ** int(n2)
+        elif operator == "//":
+            result = int(n1) // int(n2)
+        elif operator == "%":
+            result = int(n1) % int(n2)
+        else:
+            print("Invalid operator. Exiting...")
+            exit(1)
+    # Print result
+    print("Result:", result)
+
+Run()
